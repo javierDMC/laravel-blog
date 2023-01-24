35b5fa0a191b172535e2b73f5863ae0e0da7fe99
javierDMC <javierdelmoralcamacho@gmail.com>
2023-01-24 17:44:59 +0100

    Routes web.php

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,23 @@ Route::get('/', function () {
 Route::get('/posts', function () {
     echo ("Listado de posts");
 });
+
+//rutas con parametros validados
+// Route::get('saludo/{nombre?}', function ($nombre="Invitado") {
+//     return "Hola, " . $nombre;
+// })->where('nombre',"[A-Za-z]+");
+
+//rutas con nombre o named routes
+Route::get('contacto', function () {
+    return "Página de contacto";
+})->name('ruta_contacto');
+
+//combinacion de elementos en rutas
+Route::get(
+    'saludo/{nombre?}/{id?}',
+    function ($nombre = "Invitado", $id = 0) {
+        return "Hola $nombre, tu código es el $id";
+    }
+)->where('nombre', "[A-Za-z]+")
+    ->where('id', "[0-9]+")
+    ->name('saludo');
